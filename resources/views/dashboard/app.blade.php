d06652fc7b75a81b08d0e587d58e4ea54d6dc0b5
marcos <marcos@Marcos.Marcos>
2024-11-19 17:15:15 -0400

    modularizacion2

--- resources/views/dashboard/app.blade.php
+++ resources/views/dashboard/app.blade.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
-<html class="no-js" lang="zxx">
+<html class="no-js" lang="es">
 
 <head>
     <!-- Meta Tags -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="keywords" content="Site keywords here">
-    <meta name="description" content="#">
+    <meta name="description" content="Capacitación en tecnologías innovadoras">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Site Title -->
     <title>Capasitacion</title>
 
     <!-- Fav Icon -->
-    <link rel="icon" href="img/favicon.png">
+    <link rel="icon" href="{{ asset('img/favicon.png') }}">
 
     <!-- NFTMax Stylesheet -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
@@ -27,7 +27,7 @@
 
 <body>
 
-    <div class="body-bg" style="background-image:url('img/body-bg.jpg')">
+    <div class="body-bg" style="background-image:url('{{ asset('img/body-bg.jpg') }}')">
 
         <!--sidevar -->
         @include('dashboard.sidevar')
